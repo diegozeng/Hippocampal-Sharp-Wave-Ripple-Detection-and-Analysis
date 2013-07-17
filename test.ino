971ac4474d3b498aa51e78e0f4017c1a67aa605f
diegozeng <403994814@qq.com>
2013-07-17 19:24:18 +0800

    Create test.ino

---
+++ test.ino
@@ -0,0 +1,43 @@
+#include "LowPassFilter.h"
+#include "LowPassFilter.c"
+
+//The number of samples to buffer before analyzing them
+int samplesN = 1000;
+int micPin = 0;
+
+LowPassFilter* filter;
+
+void setup(){
+  analogReadResolution(12);
+  ADC->ADC_MR |= 0x800;
+  Serial.begin(115200);
+  filter = new LowPassFilter();
+  LowPassFilter_init(filter);
+}
+
+void loop(){
+  long pow = 0;
+  long filtpow = 0;
+  int peak = 0;
+  long start = millis();
+  for(int k=0; k<samplesN; k++){
+    int val = analogRead(micPin);
+    LowPassFilter_put(filter, val);
+
+    int filtered = LowPassFilter_get(filter);
+
+    pow += ((long)val*(long)val)/samplesN;
+    filtpow += ((long)filtered*(long)filtered)/samplesN;
+    peak = max(peak, filtered);
+  }
+  long end = millis(); 
+  float freq = ((float)samplesN * (float)1000) / ((float)end - (float)start);
+
+  Serial.print(freq);
+  Serial.print(" ");
+  Serial.print(pow);
+  Serial.print(" ");
+  Serial.print(filtpow);
+  Serial.print(" ");
+  Serial.println(peak);
+}
