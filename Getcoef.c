6d587d3389b74a3ba6f4324f5a60d92614268a32
diegozeng <403994814@qq.com>
2013-07-22 21:06:28 +0800

    Update Getcoef.c
    add some instructions to transfer float variables to int variables. Which can be applied in "BandPassFilter.h" directly.

--- Getcoef.c
+++ Getcoef.c
@@ -14,7 +14,7 @@
 #define PI 3.1415926535 
 
 int main(void)
-{  
+{	
 	int a=1;
 	while(a)
 	{
@@ -22,11 +22,14 @@ int main(void)
 		float N,BW;
 		float b;
 		float c,d;
+		int *W = 0;
 		float *X = 0;
 		float EPS = 0.00001;
 		int res,res1;
-    
+		//int k = 0;
+		//label1:
 		printf("Please type the normalized main lobe (0 to 1) width of the filter\n");
+		//scanf("%f",&BW);
 		res = scanf("%f",&BW);
 		getchar();
 		while(!res)
@@ -36,7 +39,6 @@ int main(void)
 				res = scanf("%f",&BW);
 				getchar();
 			}
-      
 		printf("Please type the normalized band-width (0 to 1) of the filter\n");
 		res1 = scanf("%f",&b);
 		getchar();
@@ -47,31 +49,41 @@ int main(void)
 				res1 = scanf("%f",&b);
 				getchar();
 			}
-      
-		c=PI*(1.0-b)/2;
-		d=PI-c;
+		//printf("%f",b);
+		c = PI*(1.0 - b) / 2;
+		d = PI-c;
 		//printf("%f\n",c);
 		//printf("%f\n",d);
-		N=4/BW-1;
-		N=ceil(N);
-		i=N;
-		i=i+1;	
+		N = 4 / BW - 1;
+		N = ceil(N);
+		i = N;
+		i = i + 1;	
 		printf("The window length should be %d\n",i);
+		W = (int*)malloc(sizeof(int)*i);
+		memset(W, 0, sizeof(int)*i);
 		X = (float*)malloc(sizeof(float)*i);
 		memset(X, 0, sizeof(float)*i);
-		///////////////////////////////////////  build rectangular window coefficient
+		/////////////////////////////////////// build rectangular window coefficient
 		printf("The window coefficient is\n");
 		for (j = 0;j < i;j++)
-                {
-		    X[j]=X[j]+j;
+              {
+				//W[j]=W[j]+1;
+				//printf("%d",k);
+				X[j] = X[j]+j;
+				//printf("\n");
 				if (fabs(X[j]-N/2.0) < EPS)
-				  	X[j]=(d-c)/PI;
+					X[j] = (d-c)/PI;
 				else
-					X[j]=sin(d*(X[j]-N/2.0))/(PI*(X[j]-N/2.0))-sin(c*(X[j]-N/2.0))/(PI*(X[j]-N/2.0));
-				printf("%f",X[j]);
+					X[j] = sin(d*(X[j]-N/2.0))/(PI*(X[j]-N/2.0))-sin(c*(X[j]-N/2.0))/(PI*(X[j]-N/2.0));
+				X[j]= X[j]*pow(2,32);
+				W[j]= (int)(X[j]+0.5);
+				printf("%d",W[j]); 
 				printf("\n");
-                }
-                //////////////////////////////////////
+				//printf("%f",X[j]);
+				//printf("\n");
+              }
+		///////////////////////////////////////
+		//printf("The length is %d\n",i);
 		printf("again?(Type 0 to exit)");
 		scanf("%d",&a);
 		getchar();
