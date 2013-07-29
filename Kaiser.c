4a528969d4b8b2a4f9126873154d142b7dbd6ae7
diegozeng <403994814@qq.com>
2013-07-30 07:14:05 +0800

    Update Kaiser.c

--- Kaiser.c
+++ Kaiser.c
@@ -5,10 +5,10 @@
 #include <conio.h>
 #define PI 3.1415926535 
 
-static float bessel0(float x)
+static double bessel0(double x)
 {
-  int i;
-	float d,y,d2,sum;
+	int i;
+	double d,y,d2,sum;
 	y=x/2.0;
 	d=1.0;
 	sum=1.0;
@@ -23,38 +23,44 @@ static float bessel0(float x)
 	return(sum);
 }
 
- //int main(void)
- int kaiser(int *Z)
+int main(int *W)
+//int KAIS(int *W)
 {
+	
 	int a = 1;
+	FILE * fp = fopen("data.txt", "w");
+	if(fp == NULL)
+	{	
+		printf("open file wrong!\n");
+	}
 	while(a)
 	{
 	int j;
-	float EPS = 0.00001;
-	float *beta = 0;
-	float *beta1 = 0;
-	float *win = 0;
-	float *X = 0;
-	float *Y = 0;
-	int *W = 0;
-	float *Z = 0;
-	float be,fs1,fp1,fp2,fs2,delta,alfa,N,M,fs;
+	double EPS = 0.00001;
+	double *beta = 0;
+	double *beta1 = 0;
+	double *win = 0;
+	double *X = 0;
+	double *Y = 0;
+//	int *W = 0;
+	double *Z = 0;
+	double be,fs1,fp1,fp2,fs2,delta,alfa,N,M,fs;
 	//int res2;
 	printf("pls input first sidelobe attenuation:\n");
-	scanf("%f",&alfa);
+	scanf("%lf",&alfa);
 	printf("pls input sampling frequency fs:\n");
-	scanf("%f",&fs);
+	scanf("%lf",&fs);
 	printf("pls input fs1:\n");
-	scanf("%f",&fs1);
+	scanf("%lf",&fs1);
 	fs1=fs1/fs;
 	printf("pls input fp1:\n");
-	scanf("%f",&fp1);
+	scanf("%lf",&fp1);
 	fp1=fp1/fs;
 	printf("pls input fp2:\n");
-	scanf("%f",&fp2);
+	scanf("%lf",&fp2);
 	fp2=fp2/fs;
 	printf("pls input fs2:\n");
-	scanf("%f",&fs2);
+	scanf("%lf",&fs2);
 	fs2=fs2/fs;
 	
 	if ((fp1 - fs1) > (fs2 - fp2))
@@ -62,13 +68,14 @@ static float bessel0(float x)
 	else 
 		delta = fp1-fs1;
 	M = (alfa-7.95)/(14.36*delta)+1;
-	N = ceil(M); 
+	N = ceil(M);
+	fprintf(fp, "%lf,", N);
 	printf("\n");
 	printf("Output parameters:\n");
 	printf("The filter's order is:\n");
-	printf("%f\n",N-1);
+	printf("%lf\n",N-1);
 	printf("The window length is:\n");
-	printf("%f\n",N);
+	printf("%lf\n",N);
 	if (alfa>=50.0)
 		be = 0.1102*(alfa-8.7);
 	if (21.0<alfa<50.0)
@@ -88,53 +95,56 @@ static float bessel0(float x)
 		//printf("%f",b);
 	//	c = PI*(1.0 - b) / 2;
 	//	d = PI-c;
-	beta = (float*)malloc(sizeof(float)*N);
-		memset(beta, 0, sizeof(float)*N);
-	beta1 = (float*)malloc(sizeof(float)*N);
-		memset(beta1, 0, sizeof(float)*N);
-	win = (float*)malloc(sizeof(float)*N);
-		memset(win, 0, sizeof(float)*N);
+	beta = (double*)malloc(sizeof(double)*N);
+		memset(beta, 0, sizeof(double)*N);
+	beta1 = (double*)malloc(sizeof(double)*N);
+		memset(beta1, 0, sizeof(double)*N);
+	win = (double*)malloc(sizeof(double)*N);
+		memset(win, 0, sizeof(double)*N);
 	W = (int*)malloc(sizeof(int)*N);
 		memset(W, 0, sizeof(int)*N);
-    X = (float*)malloc(sizeof(float)*N);
-		memset(X, 0, sizeof(float)*N);
-    Y = (float*)malloc(sizeof(float)*N);
-		memset(Y, 0, sizeof(float)*N);
-	Z = (float*)malloc(sizeof(float)*N);
-		memset(Z, 0, sizeof(float)*N);
+    X = (double*)malloc(sizeof(double)*N);
+		memset(X, 0, sizeof(double)*N);
+    Y = (double*)malloc(sizeof(double)*N);
+		memset(Y, 0, sizeof(double)*N);
+	Z = (double*)malloc(sizeof(double)*N);
+		memset(Z, 0, sizeof(double)*N);
 	printf("The impulse response h[n] are:\n");
 	for(j = 0; j <N; j++)
 		{
 			X[j]=X[j]+j;
 			//printf("%d\n",X[j]);
 			beta[j]=beta[j]+be;
-			//printf("%f\n",beta[j]);
+			//printf("%lf\n",beta[j]);
 			beta1[j]=beta[j]*sqrt(1.0-((2.0*X[j])/(N-1.0)-1.0)*((2.0*X[j])/(N-1.0)-1.0));
-			//printf("%f\n",beta1[j]);
+			//printf("%lf\n",beta1[j]);
 			beta1[j] = bessel0(beta1[j]);
-			//printf("%f\n",beta1[j]);
+			//printf("%lf\n",beta1[j]);
 			beta[j] = bessel0(beta[j]);
-			//printf("%f\n",beta[j]);
+			//printf("%lf\n",beta[j]);
 			win[j]=beta1[j]/beta[j];
-			//printf("%f\n",win[j]);
+			//printf("%lf\n",win[j]);
 				//printf("\n");
 			Y[j]=Y[j]+j;
 				if (fabs(Y[j]-(N-1.0)/2.0) < EPS)
 					Y[j] = fs2+fp2-fs1-fp1;
 				else
 					Y[j] = sin((PI*fs2 + PI*fp2)*(Y[j]-(N-1.0)/2.0))/(PI*(Y[j]-(N-1.0)/2.0))-sin((PI*fs1 + PI*fp1)*(Y[j]-(N-1.0)/2.0))/(PI*(Y[j]-(N-1.0)/2.0));
-				//printf("%f\n",Y[j]);
+				//printf("%lf\n",Y[j]);
 				Z[j] = Y[j]*win[j];
-				printf("%f\n",Z[j]);
+				//printf("%.15lf\n",Z[j]);
+				//fprintf(fp, "%.15lf,",Z[j]);
+				Z[j]= Z[j]*pow(2.0,32);
+				W[j]= (int)(Z[j]+0.5);
+				printf("%d\n",W[j]);
+				fprintf(fp, "%d,",W[j]);
 		}
 		printf("\n");
 		printf("again?(Type 0 to exit)");
 		scanf("%d",&a);
 		getchar();
 	}
-//	return 0;
-// Z transfer to int result
-//	result[i]
-	return N;
+	fclose(fp);
+	return 0;
 }
 
