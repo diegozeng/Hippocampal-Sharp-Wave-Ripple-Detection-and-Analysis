54ded792115288dbec9ff3a3cc78e5b53065989c
diegozeng <403994814@qq.com>
2013-08-09 22:06:16 +0800

    Create PowerPeriodic.ino

---
+++ PowerPeriodic.ino
@@ -0,0 +1,54 @@
+#include "BandPassFilter.h"
+#include "BandPassFilter.c"
+
+//The length of window to calculate power.
+int samplesN = 50;
+int micPin = 0;
+int j = 0;
+int buf[1180] = {0};
+BandPassFilter* filter;
+
+void setup()
+{
+  analogReadResolution(12);
+  analogWriteResolution(12);
+  ADC->ADC_MR = 0x10380400;
+  Serial.begin(115200);
+  filter = new BandPassFilter();
+  BandPassFilter_init(filter);
+  attachInterrupt(3,interr,FALLING);
+}
+
+void loop()
+{
+  long filtpow = 0;
+  //long start = millis();
+  for(int k=0; k<samplesN; k++)
+    {
+    int val = analogRead(micPin);
+    //var=(int)((val*3.3-2252.8)/2.2-1177);
+    BandPassFilter_put(filter, val);
+    int filtered = BandPassFilter_get(filter);
+    filtpow += ((long)filtered*(long)filtered)/samplesN;
+    
+        //peak = max(peak, filtered);
+    }
+  //long end = millis();
+  filtpow = (int)sqrt(filtpow);
+  buf[j] = filtpow;
+  analogWrite(DAC1,filtpow); 
+  j=j+1;
+  if(j==1179)
+  {
+    j=0;
+  }
+  //float freq = ((float)samplesN * (float)1000) / ((float)end - (float)start);
+  // Serial.println(freq);
+
+}
+
+void interr()
+{
+  for (int i = 0; i < j; i++)
+  Serial.println(buf[i]);
+}
